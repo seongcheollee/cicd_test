3a48ee4f4c0ced4622ebbd7b5364aefaedee2b75
seongcheollee <jhk6520736@gmail.com>
2023-11-10 00:03:51 +0900

    test

--- src/main/java/com/example/controller/SimpleController.java
+++ src/main/java/com/example/controller/SimpleController.java
@@ -1,14 +1,19 @@
 package com.example.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
+    private static final Logger logger = LoggerFactory.getLogger(SimpleController.class);
 
     @RequestMapping("/sample")
     public String greeting(){
+        logger.debug("SampleController's greeting method called");
+
         return "sample!!";
     }
 
